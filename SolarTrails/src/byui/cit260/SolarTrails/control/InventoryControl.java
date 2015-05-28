bf9e317899c52b2e2386666df3a15c1974316c71
Jeffery Davis <jtdavis247@gmail.com>
2015-05-28 10:35:49 -0600

    5/28/2015 adds all control layer classes. I also worked within the inventory controller and added the food used function. It still needs unit tests

---
+++ SolarTrails/src/byui/cit260/SolarTrails/control/InventoryControl.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.SolarTrails.control;
+
+/**
+ *
+ * @author jeff
+ */
+public class InventoryControl {
+    
+    public double calcFoodUsed(double timeLapsed, int crewSize, double foodUsed, int food) {
+       
+      if (timeLapsed > 0 && crewSize > 0) {
+            foodUsed = (timeLapsed * crewSize);
+       }
+      if  (foodUsed > food) {
+            return -1;
+       }
+      else
+            return foodUsed;
+        
+    }
+}
+/*modifiers returnType functionName ()
+calcFoodUsed(timeLapsed, crewSize , food) : double
+BEGIN
+
+END
+*/
