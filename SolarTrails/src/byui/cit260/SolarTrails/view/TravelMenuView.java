07c28d13ad780badf172d45c5b2019f5e87512f3
Jeffery Davis <jtdavis247@gmail.com>
2015-06-20 00:38:34 -0600

    6/20/2015 Adds  travelmenuview

---
+++ SolarTrails/src/byui/cit260/SolarTrails/view/TravelMenuView.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.SolarTrails.view;
+
+import byui.cit260.SolarTrails.control.GameControl;
+import java.util.Scanner;
+import solartrails.SolarTrails;
+
+/**
+ *
+ * @author jeff
+ */
+public class TravelMenuView extends View{
+    
+    public TravelMenuView(){
+    super( "\n"
+            + "\n------------------------------------"
+            + "\n  Where would you like to Travel to?                "
+            + "\n------------------------------------"
+            + "\n| Ship Menu                        |"
+            + "\n------------------------------------"
+            + "\nF - place one"
+            + "\nD - place two"
+            + "\nA - place three"
+            + "\nQ - Quit"
+            + "\n------------------------------------");
+    }
+    
+    @Override
+    public boolean doAction(Object obj) {
+        
+        String value = (String) obj;
+        value = value.toUpperCase();
+        char choice = value.charAt(0);
+        
+        switch (choice) {
+            case 'F':
+                this.displayLocationMenu();
+                break;
+            case 'D':
+                this.displayLocationMenu();
+                break;
+            case 'A':
+                this.displayLocationMenu();
+                break;
+            case 'Q':
+                return true;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        return false;
+    }
+
+    private void displayLocationMenu() {
+        System.out.println("*** displayLocationMenu function called ***");
+    }
+    
+}
